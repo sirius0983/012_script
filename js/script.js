1a8f4d9405c8b3a24b1ee60f6312cb5cc6e550fd
Rustam Siraev <sirius0983@mail.ru>
2021-02-04 10:17:37 +0500

    final-015

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 "use strict";
-// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
-//     personalMovieDB = {
-//         count: numberOfFilms,
-//         movies: {},
-//         actors: {},
-//         genres: [],
-//         privat: false
-//     },
-//     a = prompt('Один из последних просмотренных фильмов?', ''),
-//     b = prompt('На сколько оцените его?', ''),
-//     c = prompt('Один из последних просмотренных фильмов?', ''),
-//     d = prompt('На сколько оцените его?', '');
-// personalMovieDB.movies [a] = b;
-// personalMovieDB.movies [c] = d;
-// console.log (personalMovieDB);
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+    personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+for (let i = 0; i < 2; i++) {
+        let a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+        if (a != null && b != null && a !== '' && b !== '' && a.length < 50 && b.length < 50) {
+                personalMovieDB.movies [a] = b;
+                console.log('done');
+        } else {
+                console.log('error');
+                i--;
+        }
+}
+if (personalMovieDB.count < 10) {
+        console.log ("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+} else {
+       console.log("Произошла ошибка");
+}
+console.log (personalMovieDB);
 
 /* Задание на урок:
 
@@ -28,6 +43,4 @@
 "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
 4) Потренироваться и переписать цикл еще двумя способами*/
-
-
 
